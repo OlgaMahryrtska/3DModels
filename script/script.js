e6954b1f544e2506dde431785003b880f5da2c1f
Olga Magrytska <olia.magritskaia@gmail.com>
2021-04-25 16:54:30 +0200

    обязательное задание

---
+++ script/script.js
@@ -0,0 +1,39 @@
+window.addEventListener("DOMContentLoaded", () => {
+  "use strict";
+  function countTimer(deadline) {
+    const timerHours = document.querySelector("#timer-hours"),
+      timerMinutes = document.querySelector("#timer-minutes"),
+      timerSeconds = document.querySelector("#timer-seconds");
+
+    function getTimeRemaining() {
+      let dateStop = new Date(deadline).getTime(),
+        dateNow = new Date().getTime(),
+        timeRemaining = (dateStop - dateNow) / 1000,
+        seconds = Math.floor(timeRemaining % 60),
+        minutes = Math.floor((timeRemaining / 60) % 60),
+        hours = Math.floor(timeRemaining / 60 / 60);
+
+      hours = hours < 10 ? "0" + hours : hours;
+      minutes = minutes < 10 ? "0" + minutes : minutes;
+      seconds = seconds < 10 ? "0" + seconds : seconds;
+
+      return { timeRemaining, hours, minutes, seconds };
+    }
+    function updateClock() {
+      const timer = getTimeRemaining();
+      timerHours.textContent = timer.hours;
+      timerMinutes.textContent = timer.minutes;
+      timerSeconds.textContent = timer.seconds;
+      if (timer.timeRemaining > 0) {
+        setInterval(updateClock, 1000);
+      } else {
+        timerHours.textContent = "00";
+        timerMinutes.textContent = "00";
+        timerSeconds.textContent = "00";
+      }
+    }
+    updateClock();
+  }
+  countTimer("27 april 2021");
+  // setInterval(countTimer, 1000);
+});
