62fd91b1bbbff30c6490bada7b2c583f5111abbe
Olga Magrytska <olia.magritskaia@gmail.com>
2021-04-27 19:20:57 +0200

    обязательное задание

--- script/script.js
+++ script/script.js
@@ -41,24 +41,31 @@ window.addEventListener("DOMContentLoaded", () => {
   const toggleMenu = () => {
     const btnMenu = document.querySelector(".menu"),
       menu = document.querySelector("menu"),
-      closeBtn = document.querySelector(".close-btn"),
-      menuItems = menu.querySelectorAll("ul>li");
+      menuItems = menu.querySelectorAll("*");
 
     const handlerMenu = () => {
       menu.classList.toggle("active-menu");
     };
     btnMenu.addEventListener("click", handlerMenu);
-    closeBtn.addEventListener("click", handlerMenu);
 
-    menuItems.forEach((elem) => elem.addEventListener("click", handlerMenu));
+    menuItems.forEach((elem) =>
+      elem.addEventListener("click", (event) => {
+        let target = event.target;
+        console.log(target);
+        if (target.classList.contains("close-btn")) {
+          handlerMenu();
+        } else {
+          handlerMenu();
+        }
+      })
+    );
   };
   toggleMenu();
 
   //popup
   const togglePopUp = () => {
     const popup = document.querySelector(".popup"),
-      popupBtn = document.querySelectorAll(".popup-btn"),
-      popupClose = document.querySelector(".popup-close");
+      popupBtn = document.querySelectorAll(".popup-btn");
 
     popupBtn.forEach((elem) => {
       elem.addEventListener("click", () => {
@@ -66,12 +73,20 @@ window.addEventListener("DOMContentLoaded", () => {
       });
     });
 
-    popupClose.addEventListener("click", () => {
-      popup.style.display = "none";
+    popup.addEventListener("click", (event) => {
+      let target = event.target;
+      if (target.classList.contains("popup-close")) {
+        popup.style.display = "none";
+      } else {
+        target = target.closest(".popup-content");
+        if (!target) {
+          popup.style.display = "none";
+        }
+      }
     });
   };
   togglePopUp();
-  //popup animation
+  // popup animation
   const timer = setInterval(function () {
     timePassed = Date.now() - start;
     if (timePassed >= 6000) {
@@ -82,14 +97,42 @@ window.addEventListener("DOMContentLoaded", () => {
       clearTimeout(timer);
       return false;
     }
-
     animate(timePassed);
   }, 3000);
   const animate = () => {
     timePassed++;
     const popup = document.querySelector(".popup");
-    popup.style.position = "relative";
-    popup.style.display = "inline";
-    popup.style.top = timePassed + "px";
+    popup.style.display = "block";
   };
+
+  //tabs
+
+  const tabs = () => {
+    const tabHeader = document.querySelector(".service-header"),
+      tab = tabHeader.querySelectorAll(".service-header-tab"),
+      tabContent = document.querySelectorAll(".service-tab");
+    const toggleTabContent = (index) => {
+      for (let i = 0; i < tabContent.length; i++) {
+        if (index === i) {
+          tab[i].classList.add("active");
+          tabContent[i].classList.remove("d-none");
+        } else {
+          tab[i].classList.remove("active");
+          tabContent[i].classList.add("d-none");
+        }
+      }
+    };
+    tabHeader.addEventListener("click", (event) => {
+      let target = event.target;
+      target = target.closest(".service-header-tab");
+      if (target) {
+        tab.forEach((item, i) => {
+          if (item === target) {
+            toggleTabContent(i);
+          }
+        });
+      }
+    });
+  };
+  tabs();
 });
